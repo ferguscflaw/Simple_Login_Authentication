fbdece625bc4722b7ddc906282ee867c751edc6d
Fergus Law <fergus@zenithlaw.com>
2024-08-29 16:12:42 +0100

    Add authentication system

--- public/login.js
+++ public/login.js
@@ -1,3 +1,29 @@
 function format(elem){
     elem.value = elem.value.replaceAll("&", "")
 }
+
+window.onload = function(){
+    document.getElementById("goto_signup").addEventListener("click", function(){
+        document.getElementById("login").style.display = "none"
+        document.getElementById("signup").style.display = "block"
+    }, false)
+
+    document.getElementById("goto_login").addEventListener("click", function(){
+        document.getElementById("signup").style.display = "none"
+        document.getElementById("login").style.display = "block"
+    }, false)
+
+    document.getElementById("signup_btn").addEventListener("click", function(){
+        let username = document.getElementById("signup_username").value
+        let password = document.getElementById("signup_password").value
+        fetch(window.location.origin+"/signup/?"+username+"&&&&&"+password)
+            .then((response) => response.json())
+            .then((data) => alert(data.message))
+    }, false)
+
+    document.getElementById("login_btn").addEventListener("click", function(){
+        let username = document.getElementById("username").value
+        let password = document.getElementById("password").value
+        window.location.href = window.location.origin + "/?" + username + "&&&&&" + password
+    }, false)
+}
--- server.js
+++ server.js
@@ -4,13 +4,42 @@ const fs = require("fs")
 const server = express()
 
 server.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    if (req.url.includes("?")){
+        let username = req.url.split("?")[1].split("&&&&&")[0]
+        let password = req.url.split("?")[1].split("&&&&&")[1]
+        fs.readFile("./users.txt", "utf8", function(err, data){
+            if (err){
+                throw err
+            }
+            if (data.toString().split("\n").findIndex((user) => user == username+"&&&"+password) !== -1){
+                res.sendFile(path.join(__dirname, 'public', 'secret.html'))
+            }
+            else{
+                res.sendFile(path.join(__dirname, 'public', 'index.html'))
+            }
+        })
+    }
+    else{
+        res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    }
 })
 server.get("/login.js", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.js'))
 })
 server.get("/login.css", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.css'))
+})
+
+server.get("/signup/", (req, res) => {
+    let username = req.url.split("?")[1].split("&&&&&")[0]
+    let password = req.url.split("?")[1].split("&&&&&")[1]
+    fs.appendFile("./users.txt", username+"&&&"+password+"\n", (err) => {
+        if (err){
+            res.json({ status: 500, message: err })
+            throw err
+        }
+        res.json({ status: 200, message: "Success. User has been added to the system"})
+    })
 })
 
 server.get("/*", (req, res) => {
